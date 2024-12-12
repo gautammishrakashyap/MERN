39d771acb81ac3358d4e56e02abdb48077303b15
gautammishrakashyap <kumargautam7071@gmail.com>
2024-12-12 19:34:22 +0530

    error solved

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,11 +8,26 @@ const app = express();
 
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json())
+// Middleware to parse incoming JSON requests
+app.use(express.json());
 
+// Define routes
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-    console.log("Server is running on PORT~ " + PORT);
+// Start the server and connect to the database
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT~ ${PORT}`);
     connectDB();
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    app.close(() => process.exit(1));
+});
+
+// Handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    app.close(() => process.exit(1));
 });
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
-import { protectRoute } from "../middleware/auth.middleware.js";
+import { login, logout, signup, updateProfile } from "../controllers//auth.controllers.js";
+
 
 const router = express.Router();
 
@@ -8,8 +8,8 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
-router.put("/update-profile", protectRoute, updateProfile);
+router.put("/update-profile",updateProfile);
 
-router.get("/check", protectRoute, checkAuth);
+
 
 export default router;
