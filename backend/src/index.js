8954b4dc0a6b66542c7274b3dbb3f50da193a8b5
gautammishrakashyap <kumargautam7071@gmail.com>
2024-12-13 21:32:16 +0530

    finish

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,36 +1,58 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
-// import messageRoutes from "./routes/message.route.js"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const app = express();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // Middleware to parse incoming JSON requests
 app.use(express.json());
-// app.use(cookieParser());
+app.use(cookieParser());
+
+// CORS configuration
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Update as per frontend origin
+    credentials: true,
+  })
+);
 
 // Define routes
 app.use("/api/auth", authRoutes);
-// app.use("/api/message", messageRoutes)
+app.use("/api/messages", messageRoutes);
+
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // Start the server and connect to the database
-app.listen(PORT, () => {
-    console.log(`Server is running on PORT~ ${PORT}`);
-    connectDB();
+server.listen(PORT, () => {
+  console.log(`Server is running on PORT: ${PORT}`);
+  connectDB();
 });
 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
-    console.log(`Error: ${err.message}`);
-    app.close(() => process.exit(1));
+  console.log(`Unhandled Rejection: ${err.message}`);
+  server.close(() => process.exit(1));
 });
 
 // Handle uncaught exceptions
 process.on("uncaughtException", (err) => {
-    console.log(`Error: ${err.message}`);
-    app.close(() => process.exit(1));
+  console.log(`Uncaught Exception: ${err.message}`);
+  server.close(() => process.exit(1));
 });
