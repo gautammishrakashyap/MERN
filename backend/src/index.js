d8d888078591d4621f923d6ca4a62429b828920d
gautammishrakashyap <kumargautam7071@gmail.com>
2024-12-13 14:30:38 +0530

    frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+// import messageRoutes from "./routes/message.route.js"
 
 dotenv.config();
 const app = express();
